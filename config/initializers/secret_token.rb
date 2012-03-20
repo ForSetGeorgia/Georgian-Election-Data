ca1d1199372063d4f3c7fbd5da41a0bdc12a4d70
Jason <jason.addie@gmail.com>
2012-03-20 13:41:42 +0400

    - created basic rails app - updated gem file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElectionMap::Application.config.secret_token = '8dd2b6351be0cf54f70f9d182811ac87f89e96f78201461ccda8e825685a2a1c9b9dc68f74d64df9e661fa3b048eb62182d58ca89f141b360fb4476a8ea89a6c'
