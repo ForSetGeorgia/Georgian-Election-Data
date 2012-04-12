81130f00005dc27f500d2407b960acea87557a8a
Eric Barrett <ericnbarrett@gmail.com>
2012-04-12 11:51:21 +0400

    capified application for deployment adding deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,15 @@
+set :application, "Election-Map"
+set :repository,  "git@github.com:JumpStartGeorgia/Election-Map.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "192.168.1.117"
+role :app, "192.168.1.117"
+role :db,  "192.168.1.117", :primary => true
+
+set :deploy_to, "/home/deployer"
+set :user, "deployer"
+set :scm_username, "ericnbarrett"
+
+server "election-map.jumpstart.ge", :app, :web, :db, :primary => true
