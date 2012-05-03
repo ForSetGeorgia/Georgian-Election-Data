5522c0484de2e25829202866582aa8c8129b5563
Jason <jason.addie@gmail.com>
2012-05-03 18:59:52 +0400

    added test to see if locale is valid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,23 @@ class ApplicationController < ActionController::Base
 protected 
 
   def set_locale
-    I18n.locale = params[:locale] if params[:locale]
-    @locales = Locale.all
+    @locales = Locale.order("language asc")
+		# see if locale is valid
+		valid = false
+		if !params[:locale].nil?
+			@locales.each do |l|
+				if l.language.downcase == params[:locale].downcase
+					valid = true
+					break
+				end
+			end
+			if valid
+			  I18n.locale = params[:locale] if params[:locale]
+			else
+			  I18n.locale = I18n.default_locale
+			  params[:locale] = I18n.default_locale
+			end
+		end
   end
   
   def set_election_types
