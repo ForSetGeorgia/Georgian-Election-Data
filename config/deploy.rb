2346dbbb6b335f41e55087b633cd10d15056ed96
Eric Barrett <ericnbarrett@gmail.com>
2012-06-04 12:45:11 +0400

    updated deploy.rb for rvm

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,12 @@
 require "bundler/capistrano"
 
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+# Load RVM's capistrano plugin.    
+require "rvm/capistrano"
+
+set :rvm_ruby_string, '1.9.2'
+
 server "alpha.jumpstart.ge", :web, :app, :db, primary: true
 
 set :application, "Election-Map"
