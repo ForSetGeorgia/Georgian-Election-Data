52b086f601e143c74e40791bb48e6b399b78a3e1
Jason <jason.addie@gmail.com>
2012-06-08 12:10:14 +0400

    more deploy changes - cannot use . in variable name

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -4,7 +4,7 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.Election-Map.sock"
+listen "/tmp/unicorn_Election-Map.sock"
 listen 8081, :tcp_nopush => true # must be unique port # for each app
 worker_processes 2
 timeout 30
