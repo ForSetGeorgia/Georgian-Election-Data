23d629531ff694faa4958c05a262cb9970a6c747
Jason <jason.addie@gmail.com>
2012-08-10 16:02:31 +0400

    changed cache pages to use flash messages

--- app/controllers/cache_controller.rb
+++ app/controllers/cache_controller.rb
@@ -2,23 +2,38 @@ class CacheController < ApplicationController
   before_filter :authenticate_user!
 
   def default_custom_event
-		JsonCache.default_event_cache if request.post?
+		if request.post?
+			JsonCache.default_event_cache
+			flash[:notice] = I18n.t('cache.default_custom_event.created')
+		end
   end
 
   def custom_event_indicators
-		JsonCache.custom_event_indicator_cache if request.post?
+		if request.post?
+			JsonCache.custom_event_indicator_cache
+			flash[:notice] = I18n.t('cache.custom_event_indicators.created')
+		end
   end
 
   def clear_all
-		JsonCache.clear_all if request.post?
+		if request.post?
+			JsonCache.clear_all
+			flash[:notice] = I18n.t('cache.clear_all.cleared')
+		end
   end
 
   def clear_memory
-		JsonCache.clear_cache if request.post?
+		if request.post?
+			JsonCache.clear_cache
+			flash[:notice] = I18n.t('cache.clear_memory.cleared')
+		end
   end
 
   def clear_files
-		JsonCache.clear_files if request.post?
+		if request.post?
+			JsonCache.clear_files
+			flash[:notice] = I18n.t('cache.clear_files.cleared')
+		end
   end
 
 
