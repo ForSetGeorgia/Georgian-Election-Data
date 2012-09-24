b3d2c98cbd116fe4d805dd41817adc12059a5727
Jason <jason.addie@gmail.com>
2012-09-24 10:11:49 +0400

    removed adjara from live menu; added default values for data available at

--- db/migrate/20120919072456_create_2012_elections.rb
+++ db/migrate/20120919072456_create_2012_elections.rb
@@ -22,14 +22,14 @@ class Create2012Elections < ActiveRecord::Migration
 		e.event_translations.create(:locale=>"ka", :name=>"2012 წლის აჭარის უმაღლესი საბჭოს არჩევნები - პარტიული სია
 ", :name_abbrv => '2012 პარტიული სია', :description => '2008 წლის 3 ნოემბრის აჭარის უმაღლესი საბჭოს არჩევნების შედეგები პარტიული სიით (პროპორციული წესით). აჭარის უმაღლესი საბჭოს წევრები აირჩევიან ოთხი წლის ვადით.')
 		# create live event menu record
-		MenuLiveEvent.create(:event_id => e.id, :menu_start_date => '2012-09-17', :menu_end_date => '2012-10-08')
+#		MenuLiveEvent.create(:event_id => e.id, :menu_start_date => '2012-09-17', :menu_end_date => '2012-10-08')
 
 		# 2012 adjara major
 		e = Event.create(:event_date => '2012-10-01', :event_type_id => 4, :shape_id => 65135	)
 		e.event_translations.create(:locale=>"en", :name=>"2012 Adjara Supreme Council - Majoritarian", :name_abbrv => '2012 Majoritarian', :description => 'The results of the November 3, 2008 election for majoritarian districts of  the Adjara Supreme Council. Members of the Adjara Supreme Council are elected for four year terms.')
 		e.event_translations.create(:locale=>"ka", :name=>"2012 წლის აჭარის უმაღლესი საბჭოს არჩევნები - მაჟორიტარული", :name_abbrv => '2012 მაჟორიტარული', :description => '2008 წლის 3 ნოემბრის აჭარის უმაღლესი საბჭოს არჩევნების შედეგები მაჟორიტარული წესით. აჭარის უმაღლესი საბჭოს წევრები აირჩევიან ოთხი წლის ვადით.')
 		# create live event menu record
-		MenuLiveEvent.create(:event_id => e.id, :menu_start_date => '2012-09-17', :menu_end_date => '2012-10-08')
+#		MenuLiveEvent.create(:event_id => e.id, :menu_start_date => '2012-09-17', :menu_end_date => '2012-10-08')
 
     I18n.available_locales.each do |locale|
       Rails.cache.delete("live_event_menu_json_#{locale}")
--- db/migrate/20120920202929_add_live_menu_field.rb
+++ db/migrate/20120920202929_add_live_menu_field.rb
@@ -2,6 +2,13 @@ class AddLiveMenuField < ActiveRecord::Migration
   def up
     add_column :menu_live_events, :data_available_at, :datetime
     add_index :menu_live_events, :data_available_at
+
+		# add value to each exist live menu record
+		MenuLiveEvent.all.each do |menu|
+			# time is in utc -> geo is +4
+			menu.data_available_at = '2012-10-01 21:00:00'
+			menu.save
+		end
   end
 
   def down
