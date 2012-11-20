f79df422e86e714111f0e64dd8127e19169fc0c0
Jason <jason.addie@gmail.com>
2012-11-20 10:39:36 +0400

    trying to compile assets locally and then copy to server

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,6 @@ ssh_options[:forward_agent] = true
 set :keep_releases, 2
 after "deploy", "deploy:cleanup" # remove the old releases
 
-
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
@@ -69,4 +68,35 @@ namespace :deploy do
     end
   end
   before "deploy", "deploy:check_revision"
+
+	# taken from http://www.rostamizadeh.net/blog/2012/04/14/precompiling-assets-locally-for-capistrano-deployment/
+	before 'deploy:finalize_update', 'deploy:assets:symlink'
+	after 'deploy:update_code', 'deploy:assets:precompile'
+	namespace :assets do
+    task :precompile, :roles => :web do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        puts "*****************"
+        puts "Assets have changed, compiling locally and then copying to shared/assets folder on server"
+        puts "*****************"
+        run_locally("rake assets:clean && rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+      else
+        puts "*****************"
+        puts "Skipping asset precompilation because there were no asset changes"
+        puts "*****************"
+      end
+    end
+
+    task :symlink, roles: :web do
+      run ("rm -rf #{latest_release}/public/assets &&
+            mkdir -p #{latest_release}/public &&
+            mkdir -p #{shared_path}/assets &&
+            ln -s #{shared_path}/assets #{latest_release}/public/assets")
+    end
+  end
 end
