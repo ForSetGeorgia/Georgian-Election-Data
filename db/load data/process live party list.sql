fee0190e6ba2de7997d97190b186cee2206e487c
Jason <jason.addie@gmail.com>
2012-12-26 17:42:44 +0400

    updated upload sql code to work with live and official data

--- db/load data/process live party list.sql
+++ db/load data/process live party list.sql
@@ -1,24 +1,25 @@
 use election_data-elections;
 
 # first truncate staging and raw tables
-truncate table `2012 election parl major - staging`;
-truncate table `2012 election parl major - raw`;
+truncate table `2012 election parl party list - staging`;
+truncate table `2012 election parl party list - raw`;
 
 # load data from the csv file
-LOAD DATA LOCAL INFILE '/var/www/data/major.csv'
-INTO TABLE `2012 election parl major - staging`
+LOAD DATA LOCAL INFILE '/var/www/data/live_party_list.csv'
+INTO TABLE `2012 election parl party list - staging`
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 # load data into the raw table
-insert into `2012 election parl major - raw`
+insert into `2012 election parl party list - raw`
 select distinct
 `rd`.`region` AS `region`,
 `staging`.`district_id` AS `district_id`,
 `rd`.`district_name` AS `district_name`,
 `staging`.`precinct_id` AS `precinct_id`,
+`staging`.`attached_precinct_id` AS `attached_precinct_id`,
 `staging`.`num_possible_voters` AS `num_possible_voters`,
 `staging`.`num_special_voters` AS `num_special_voters`,
 `staging`.`num_at_12` AS `num_at_12`,
@@ -43,16 +44,16 @@ select distinct
 `parties`.`Labour` AS `Labour`,
 `parties`.`Sportsman's Union` AS `Sportsman's Union`,
 `parties`.`Georgian Dream` AS `Georgian Dream`
-from ((`2012 election parl major - staging` `staging`
+from ((`2012 election parl party list - staging` `staging`
 join `regions_districts` `rd` on((`staging`.`district_id` = `rd`.`district_id`)))
-join `2012 election parl major - parties` `parties` on(((`staging`.`district_id` = `parties`.`district_id`) and (`staging`.`precinct_id` = `parties`.`precinct_id`))));
+join `2012 election parl party list - parties` `parties` on(((`staging`.`district_id` = `parties`.`district_id`) and (`staging`.`precinct_id` = `parties`.`precinct_id`))));
 
 
 # now download the data to csv
 SELECT 'shape', 'common_id', 'common_name', 'Total Turnout (#)', 'Total Turnout (%)', 'Number of Precincts with Invalid Ballots from 0-1%', 'Number of Precincts with Invalid Ballots from 1-3%', 'Number of Precincts with Invalid Ballots from 3-5%', 'Number of Precincts with Invalid Ballots > 5%', 'Invalid Ballots (%)', 'Average votes per minute (08:00-12:00)', 'Average votes per minute (12:00-17:00)', 'Average votes per minute (17:00-20:00)', 'Number of Precincts with votes per minute > 2 (08:00-12:00)', 'Number of Precincts with votes per minute > 2 (12:00-17:00)', 'Number of Precincts with votes per minute > 2 (17:00-20:00)', 'Number of Precincts with votes per minute > 2', 'Precincts Reported (#)', 'Precincts Reported (%)', 'Free Georgia', 'National Democratic Party of Georgia', 'United National Movement', 'Movement for Fair Georgia', 'Christian-Democratic Movement', 'Public Movement', 'Freedom Party', 'Georgian Group', 'New Rights', 'People\'s Party', 'Merab Kostava Society', 'Future Georgia', 'Labour Council of Georgia', 'Labour', 'Sportsman\'s Union', 'Georgian Dream'
 union
-select * from `2012 election parl major - csv` where common_id != '' && common_name != ''
-INTO OUTFILE '/var/www/data/upload_major.csv'
+select * from `2012 election parl party list - csv` where common_id != '' && common_name != ''
+INTO OUTFILE '/var/www/data/upload_live_party_list.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
---
+++ db/load data/process official major.sql
@@ -0,0 +1,31 @@
+use election_data-elections;
+
+# first truncate staging and raw tables
+truncate table `2012 election parl major - raw`;
+
+# load data from the csv file
+LOAD DATA LOCAL INFILE '/var/www/data/official_major.csv'
+INTO TABLE `2012 election parl major - raw`
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+# add the region/district names
+update `2012 election parl major - raw` as t, regions_districts as rd
+set t.region = rd.region,t.district_name = rd.district_name
+where t.district_id = rd.district_id;
+
+# add valid votes
+update `2012 election parl major - raw`
+set num_valid_votes = (num_votes-num_invalid_votes);
+
+
+# now download the data to csv
+SELECT 'shape', 'common_id', 'common_name', 'Total Turnout (#)', 'Total Turnout (%)', 'Number of Precincts with Invalid Ballots from 0-1%', 'Number of Precincts with Invalid Ballots from 1-3%', 'Number of Precincts with Invalid Ballots from 3-5%', 'Number of Precincts with Invalid Ballots > 5%', 'Invalid Ballots (%)', 'Average votes per minute (08:00-12:00)', 'Average votes per minute (12:00-17:00)', 'Average votes per minute (17:00-20:00)', 'Number of Precincts with votes per minute > 2 (08:00-12:00)', 'Number of Precincts with votes per minute > 2 (12:00-17:00)', 'Number of Precincts with votes per minute > 2 (17:00-20:00)', 'Number of Precincts with votes per minute > 2', 'Precincts Reported (#)', 'Precincts Reported (%)', 'Free Georgia', 'National Democratic Party of Georgia', 'United National Movement', 'Movement for Fair Georgia', 'Christian-Democratic Movement', 'Public Movement', 'Freedom Party', 'Georgian Group', 'New Rights', 'People\'s Party', 'Merab Kostava Society', 'Future Georgia', 'Labour Council of Georgia', 'Labour', 'Sportsman\'s Union', 'Georgian Dream'
+union
+select * from `2012 election parl major - csv` where common_id != '' && common_name != ''
+INTO OUTFILE '/var/www/data/upload_official_major.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
