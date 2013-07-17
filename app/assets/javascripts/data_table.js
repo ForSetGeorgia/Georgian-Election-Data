994358d68ff71a596f507ddf8189bb5582d397cb
jason <jason.addie@gmail.com>
2013-07-17 19:28:16 +0400

    format table to match profile; add export buttons

--- app/assets/javascripts/data_table.js
+++ app/assets/javascripts/data_table.js
@@ -1,4 +1,4 @@
-var was_selected = [];
+var was_selected_map = [];
 
 $(document).ready(function() {
 
@@ -6,14 +6,13 @@ $(document).ready(function() {
     // apply chosen jquery to filters
     $('select#data_table_filter').chosen({width: "100%"});
     // record which columns are visible by default
-    $('select#data_table_filter').find(":selected").each(function() { was_selected.push($(this).data('i')) });
+    $('select#data_table_filter').find(":selected").each(function() { was_selected_map.push($(this).data('i')) });
 
 
     // when indicator filter changes, update what indicators to show
     // - compare list of what was selected vs what is selected and only update the items that don't match
     $('select#data_table_filter').live('change', function(){
       var lookup = {};
-      var missing = [];
       var selected = [];
       
       // get all options that are selected
@@ -23,18 +22,18 @@ $(document).ready(function() {
       for (var j in selected) {
         lookup[selected[j]] = selected[j];
       }
-      for (var i in was_selected) {
-        if (typeof lookup[was_selected[i]] == 'undefined') {
+      for (var i in was_selected_map) {
+        if (typeof lookup[was_selected_map[i]] == 'undefined') {
           // item removed
-          $('table#map_data_table th[data-i="' + was_selected[i] + '"]').addClass('hidden');
-          $('table#map_data_table td[data-i="' + was_selected[i] + '"]').addClass('hidden');
+          $('table#map_data_table th[data-i="' + was_selected_map[i] + '"]').addClass('hidden');
+          $('table#map_data_table td[data-i="' + was_selected_map[i] + '"]').addClass('hidden');
         } 
       }      
        
       lookup = {};
       // find what was selected and turn col on
-      for (var j in was_selected) {
-        lookup[was_selected[j]] = was_selected[j];
+      for (var j in was_selected_map) {
+        lookup[was_selected_map[j]] = was_selected_map[j];
       }
       for (var i in selected) {
         if (typeof lookup[selected[i]] == 'undefined') {
@@ -48,7 +47,7 @@ $(document).ready(function() {
       $(this).trigger("liszt:updated");
             
       // update what items are selected
-      was_selected = selected;
+      was_selected_map = selected;
     });
   }
 });
