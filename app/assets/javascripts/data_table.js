296c34b65bdef16c13f6f9f33bc6f9a8bc5567de
jason <jason.addie@gmail.com>
2013-09-10 10:41:59 +0400

    fixed data table so all cols show when not have summary

--- app/assets/javascripts/data_table.js
+++ app/assets/javascripts/data_table.js
@@ -2,6 +2,8 @@ var was_selected_map = [];
 var table_ind_ids;
 
 function update_selected_cols(){
+console.log('=========================');
+console.log('update selected cols start');
   var lookup = {};
   var selected = [];
   var data_table = $('#map_data_table').dataTable();
@@ -9,27 +11,37 @@ function update_selected_cols(){
   
   // get all options that are selected
   $('select#data_table_filter').find(":selected").each(function() { selected.push($(this).data('id')) });
+console.log('selected = ' + selected);
+console.log('was selected = ' + was_selected_map);
 
   // find what was de-selected and turn col off
   for (var j in selected) {
     lookup[selected[j]] = selected[j];
   }
+  
+console.log('- removing items');
+  
   for (var i in was_selected_map) {
     if (typeof lookup[was_selected_map[i]] == 'undefined') {
       // item removed
+console.log(' -- removed: ' + was_selected_map[i]);
+
       index = table_ind_ids.indexOf(was_selected_map[i]);
       if (index > -1)
         data_table.fnSetColumnVis(index+1, false);
     } 
   }      
    
+console.log('- adding items');
   lookup = {};
   // find what was selected and turn col on
   for (var j in was_selected_map) {
     lookup[was_selected_map[j]] = was_selected_map[j];
   }
+
   for (var i in selected) {
     if (typeof lookup[selected[i]] == 'undefined') {
+console.log(' -- adding: ' + selected[i]);
       // item added
       index = table_ind_ids.indexOf(selected[i]);
       if (index > -1)
@@ -39,6 +51,7 @@ function update_selected_cols(){
 
   // update what items are selected
   was_selected_map = selected;
+console.log('update selected cols end');
 }
 
 $(document).ready(function() {
