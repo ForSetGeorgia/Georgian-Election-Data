54a344b68467be96eeb27b02f64e2003d6d35e2c
jason <jason.addie@gmail.com>
2013-09-18 00:41:05 +0400

    added migration to fix 2013 bi-elec description

---
+++ db/migrate/20130917203003_fix_2013_bi_elec_desc.rb
@@ -0,0 +1,24 @@
+# encoding: UTF-8
+class Fix2013BiElecDesc < ActiveRecord::Migration
+  def up
+    EventTranslation.transaction do
+      event_name = '2013 Parliamentary Bi-election - Majoritarian'
+      event = EventTranslation.where(:name => event_name)
+      if event.present?
+        event.first.description = 'The results of the April 27, 2013 elections in Bagdati district 52, Nadzaladevi district 9 and Samtredia district 54 for vacant majoritarian seats in Parliament.'
+        event.first.save
+      end
+    
+      event_name = '2013 წლის შუალედური საპარლამენტო არჩევნები - მაჟორიტარული'
+      event = EventTranslation.where(:name => event_name)
+      if event.present?
+        event.first.description = 'The results of the April 27, 2013 elections in Bagdati district 52, Nadzaladevi district 9 and Samtredia district 54 for vacant majoritarian seats in Parliament.'
+        event.first.save
+      end
+    end
+  end
+
+  def down
+    # do nothing
+  end
+end
