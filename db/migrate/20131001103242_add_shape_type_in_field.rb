58fa661d8958f55ceb193a3ea4c6412cf22cf4ba
jason <jason.addie@gmail.com>
2013-10-01 16:19:22 +0400

    more styles for news/archives; updated shape type migration

--- db/migrate/20131001103242_add_shape_type_in_field.rb
+++ db/migrate/20131001103242_add_shape_type_in_field.rb
@@ -5,16 +5,16 @@ class AddShapeTypeInField < ActiveRecord::Migration
   
     # add data
     ka_text = Hash.new
-    ka_text['თბილისი ოლქი'] = 'თბილისი ოლქი'
-    ka_text['თბილისის უბანი'] = 'თბილისის უბანი'
-    ka_text['მაჟორიტარული თბილისის ოლქი'] = 'მაჟორიტარული თბილისის ოლქი'
-    ka_text['მაჟორიტარული თბილისის უბანი'] = 'მაჟორიტარული თბილისის უბანი'
-    ka_text['მაჟორიტარული ოლქი'] = 'მაჟორიტარული ოლქი'
-    ka_text['მაჟორიტარული უბანი'] = 'მაჟორიტარული უბანი'
-    ka_text['ოლქი'] = 'ოლქი'
-    ka_text['რეგიონი'] = 'რეგიონი'
-    ka_text['უბანი'] = 'უბანი'
-    ka_text['ქვეყანა'] = 'ქვეყანა'
+    ka_text['თბილისი ოლქი'] = 'თბილისის ოლქში'
+    ka_text['თბილისის უბანი'] = 'თბილისის უბანში'
+    ka_text['მაჟორიტარული თბილისის ოლქი'] = 'თბილისის მაჟორიტარულ ოლქში'
+    ka_text['მაჟორიტარული თბილისის უბანი'] = 'თბილისის მაჟორიტარულ უბანში'
+    ka_text['მაჟორიტარული ოლქი'] = 'მაჟორიტარულ ოლქში'
+    ka_text['მაჟორიტარული უბანი'] = 'მაჟორიტარულ უბანში'
+    ka_text['ოლქი'] = 'ოლქში'
+    ka_text['რეგიონი'] = 'რეგიონში'
+    ka_text['უბანი'] = 'უბანში'
+    ka_text['ქვეყანა'] = 'ქვეყანაში'
     
     ShapeTypeTranslation.transaction do 
       ShapeTypeTranslation.all.each do |trans|
@@ -22,6 +22,21 @@ class AddShapeTypeInField < ActiveRecord::Migration
           trans.name_singular_in = 'in ' + trans.name_singular
         else
           trans.name_singular_in = ka_text[trans.name_singular]
+          
+          # fix some names while in here
+          if trans.name_singular == 'მაჟორიტარული თბილისის ოლქი'
+            trans.name_singular = 'თბილისის მაჟორიტარული  ოლქი'
+            trans.name_plural = 'თბილისის მაჟორიტარული  ოლქები'
+            trans.name_singular_possessive = 'თბილისის მაჟორიტარული ოლქის'
+          end
+          
+          if trans.name_singular == 'მაჟორიტარული თბილისის უბანი'
+            trans.name_singular = 'თბილისის მაჟორიტარული  უბანი'
+            trans.name_plural = 'თბილისის მაჟორიტარული  უბნები'
+            trans.name_singular_possessive = 'თბილისის მაჟორიტარული უბნის'
+          end
+          
+          
         end
         trans.save      
       end
