4dfea5eed7aa5d9cb6815d2cd57dbffd57404b11
jason <jason.addie@gmail.com>
2013-10-18 15:25:03 +0400

    cleaning up styles

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -3,23 +3,6 @@
 $(document).ready(function() {
   if (gon.landing_page){
 
-/*
-    // get the max height for row 3 divs and set all other divs to match
-    function adjust_landing_page_heights(){
-      var heights = [];
-      $('#row2 .row2_links').each(function(){
-        heights.push($(this).height());
-      });
-      
-      $('#row2 .row2_links').each(function() { $(this).height(Math.max.apply(Math, heights)); });
-    }
-
-    $(window).resize(function(){
-      adjust_landing_page_heights();
-    });
-
-    adjust_landing_page_heights();
-*/
     $('a.landing_circle_link').click(function(){
       landing_circle_link_obj = this;
       $.fancybox({
