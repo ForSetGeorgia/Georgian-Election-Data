43cc0731921b12fbe963469665079a5127dbeeae
jason <jason.addie@gmail.com>
2015-08-24 13:01:09 +0400

    new prod server

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -14,11 +14,11 @@ stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/#{sock_name}.sock"
 listen port_num, :tcp_nopush => true
-worker_processes 5
+worker_processes 2
 timeout tout
 
 #################################
-## the following was taken from 
+## the following was taken from
 ## https://gist.github.com/scottlowe/1303554
 #################################
 # Load app into the master before forking workers for super-fast
@@ -29,12 +29,12 @@ timeout tout
 if GC.respond_to?(:copy_on_write_friendly=)
   GC.copy_on_write_friendly = true
 end
- 
+
 before_fork do |server, worker|
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
- 
+
   ##
   # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
   # immediately start loading up a new version of itself (loaded with a new
@@ -45,9 +45,9 @@ before_fork do |server, worker|
   # we send it a QUIT.
   #
   # Using this method we get 0 downtime deploys.
- 
+
   old_pid = "#{server.config[:pid]}.oldbin"
- 
+
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
@@ -57,12 +57,12 @@ before_fork do |server, worker|
     end
   end
 end
- 
+
 after_fork do |server, worker|
   # Unicorn master loads the app then forks off workers - because of the way
   # Unix forking works, we need to make sure we aren't using any of the parent's
   # sockets, e.g. db connection
- 
+
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
   # Redis and Memcached would go here but their connections are established
   # on demand, so the master never opens a socket
